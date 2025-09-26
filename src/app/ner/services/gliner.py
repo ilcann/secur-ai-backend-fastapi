46ecc5b45b75e283fbd3659ae7907029b61e9180
ilcann <ilcan21@itu.edu.tr>
2025-09-26 13:53:11 +0300

    refactor: Simplify regex patterns in REGEX_PATTERNS by removing unused patterns

--- src/app/ner/services/gliner.py
+++ src/app/ner/services/gliner.py
@@ -35,39 +35,13 @@ LABELS = [
 ]
 
 REGEX_PATTERNS = {
-    "PERSON_NAME": re.compile(r"\b([A-Z][a-z]+(?:\s[A-Z][a-z]+)+)\b"),  # Basit isim tahmini (ilk ve soyad)
-    "ORGANIZATION": re.compile(r"\b[A-Z][A-Za-z0-9&.,\s]{2,}(?:Inc|LLC|Ltd|Corp|Company)?\b"),
     "EMAIL": re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"),
     "IP_ADDRESS": re.compile(r"\b(?:\d{1,3}\.){3}\d{1,3}\b"),
     "CREDIT_CARD_NUMBER": re.compile(r"\b(?:\d[ -]*?){13,16}\b"),
-    "CREDIT_CARD_EXPIRATION_DATE": re.compile(r"\b(0[1-9]|1[0-2])[/\-](\d{2}|\d{4})\b"),
-    "CVV": re.compile(r"\b\d{3,4}\b"),
     "PHONE_NUMBER": re.compile(r"\b(?:\+?\d{1,3})?[-.\s]?\(?\d{2,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}\b"),
-    "MOBILE_PHONE_NUMBER": re.compile(r"\b(?:\+?\d{1,3})?[-.\s]?\(?\d{2,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}\b"),
-    "ADDRESS": re.compile(r"\d{1,5}\s\w+(?:\s\w+){0,5},?\s\w+(?:\s\w+){0,3},?\s[A-Z]{2}\s\d{5}"),  # ABD tarzı adres
-    "DATE_OF_BIRTH": re.compile(r"\b(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](\d{2}|\d{4})\b"),
-    "PASSPORT_NUMBER": re.compile(r"\b[A-PR-WY][1-9]\d\s?\d{4}[1-9]\b"),
-    "BANK_ACCOUNT_NUMBER": re.compile(r"\b\d{8,20}\b"),
-    "DRIVERS_LICENSE_NUMBER": re.compile(r"\b[A-Z0-9]{5,20}\b"),
-    "TAX_IDENTIFICATION_NUMBER": re.compile(r"\b\d{9,15}\b"),
-    "IDENTITY_CARD_NUMBER": re.compile(r"\b\d{8,20}\b"),
-    "NATIONAL_ID_NUMBER": re.compile(r"\b\d{9,14}\b"),
-    "IBAN": re.compile(r"\b[A-Z]{2}\d{2}[A-Z0-9]{1,30}\b"),
-    "USERNAME": re.compile(r"\b[A-Za-z0-9._-]{3,20}\b"),
-    "HEALTH_INSURANCE_NUMBER": re.compile(r"\b[A-Z0-9]{6,20}\b"),
-    "REGISTRATION_NUMBER": re.compile(r"\b[A-Z0-9]{5,15}\b"),
-    "INSURANCE_NUMBER": re.compile(r"\b[A-Z0-9]{6,20}\b"),
-    "FLIGHT_NUMBER": re.compile(r"\b[A-Z]{2}\d{1,4}\b"),
-    "BLOOD_TYPE": re.compile(r"\b(A|B|AB|O)[+-]\b"),
-    "LICENSE_PLATE_NUMBER": re.compile(r"\b[A-Z0-9]{2,3}-[A-Z0-9]{2,4}\b"),
-    "POSTAL_CODE": re.compile(r"\b\d{5}(?:-\d{4})?\b"),
-    "SERIAL_NUMBER": re.compile(r"\b[A-Z0-9]{8,20}\b"),
-    "FAX_NUMBER": re.compile(r"\b(?:\+?\d{1,3})?[-.\s]?\(?\d{2,4}\)?[-.\s]?\d{3,4}[-.\s]?\d{3,4}\b"),
-    "VISA_NUMBER": re.compile(r"\b4[0-9]{12}(?:[0-9]{3})?\b"),
-    "TRANSACTION_NUMBER": re.compile(r"\b[A-Z0-9]{8,20}\b"),
-    "NATIONAL_HEALTH_INSURANCE_NUMBER": re.compile(r"\b[A-Z0-9]{8,20}\b"),
-    "CVC": re.compile(r"\b\d{3,4}\b"),
-    "COMPANY": re.compile(r"\b[A-Z][A-Za-z0-9&.,\s]{2,}(?:Inc|LLC|Ltd|Corp|Company)?\b"),
+    "DATE_OF_BIRTH": re.compile(r"\b\d{4}[-/]\d{2}[-/]\d{2}\b"),  # YYYY-MM-DD
+    "PASSPORT_NUMBER": re.compile(r"\b[A-PR-WY][1-9]\d\s?\d{4}[1-9]\b"),  # örnek 
+    "IBAN": re.compile(r"\b[A-Z]{2}\d{2}[ ]?(\d{4}[ ]?){4,7}\d{1,4}\b"),
 }
 
 
